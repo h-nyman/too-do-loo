3a44f23635847470474af6958ed7aba1c327ed77
Hanna Nyman <hanna.m.nyman@student.laurea.fi>
2024-10-19 14:51:45 +0300

    checklist

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("helloo");
+function addToDo(event) {
+    event.preventDefault();
+
+    const data = new FormData(event.target);
+    console.log(data);
+
+    const toDo = data.get("to-do");
+
+    const toDoNode = document.createElement("li");
+
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("type","checkbox");
+
+    const label = document.createElement("p");
+    label.appendChild(checkbox);
+
+    label.appendChild(document.createTextNode(toDo));
+
+    toDoNode.appendChild(label);
+
+    document.getElementById("to-do-list").appendChild(toDoNode);
+}
+
+window.addEventListener("load",function() {
+    document.getElementById("to-do-form").addEventListener("submit",addToDo);
+})
